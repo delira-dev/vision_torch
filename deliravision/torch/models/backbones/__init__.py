62f8ae8be14d408d4951798880864492d2c60236
mibaumgartner <m.baumgartner@ymail.com>
2019-08-13 20:30:10 +0200

    remove `get_backends` from inits

--- deliravision/torch/models/backbones/__init__.py
+++ deliravision/torch/models/backbones/__init__.py
@@ -1,31 +1,27 @@
-from delira import  get_backends as __get_backends
-
 __all__ = []
 
-if "TORCH" in __get_backends():
+from .resnet import ResNetTorch
+from .vgg import VGGTorch
+from .alexnet import AlexNetTorch
+from .squeezenet import SqueezeNetTorch
+from .densenet import DenseNetTorch
+from .mobilenet import MobileNetV2Torch
+from .resnext import ResNeXtTorch
+from .seblocks import SEBasicBlockTorch, SEBottleneckTorch, \
+    SEBottleneckXTorch
+from .unet import UNetTorch, LinkNetTorch
 
-    from .resnet import ResNetTorch
-    from .vgg import VGGTorch
-    from .alexnet import AlexNetTorch
-    from .squeezenet import SqueezeNetTorch
-    from .densenet import DenseNetTorch
-    from .mobilenet import MobileNetV2Torch
-    from .resnext import ResNeXtTorch
-    from .seblocks import SEBasicBlockTorch, SEBottleneckTorch, \
-        SEBottleneckXTorch
-    from .unet import UNetTorch, LinkNetTorch
-
-    __all__ += [
-        "AlexNetTorch",
-        "DenseNetTorch",
-        "LinkNetTorch",
-        "MobileNetV2Torch",
-        "ResNetTorch",
-        "ResNeXtTorch",
-        "SEBasicBlockTorch",
-        "SEBottleneckTorch",
-        "SEBottleneckXTorch",
-        "SqueezeNetTorch",
-        "UNetTorch",
-        "VGGTorch",
-    ]
+__all__ += [
+    "AlexNetTorch",
+    "DenseNetTorch",
+    "LinkNetTorch",
+    "MobileNetV2Torch",
+    "ResNetTorch",
+    "ResNeXtTorch",
+    "SEBasicBlockTorch",
+    "SEBottleneckTorch",
+    "SEBottleneckXTorch",
+    "SqueezeNetTorch",
+    "UNetTorch",
+    "VGGTorch",
+]
--- deliravision/torch/models/basic_networks/__init__.py
+++ deliravision/torch/models/basic_networks/__init__.py
@@ -1,13 +1,9 @@
-from delira import get_backends as __get_backends
-
 __all__ = []
 
-if "TORCH" in __get_backends():
+from .segmentation import BaseSegmentationTorchNetwork
+from .classification import BaseClassificationTorchNetwork
 
-    from .segmentation import BaseSegmentationTorchNetwork
-    from .classification import BaseClassificationTorchNetwork
-
-    __all__ += [
-        "BaseSegmentationTorchNetwork",
-        "BaseClassificationTorchNetwork"
-    ]
+__all__ += [
+    "BaseSegmentationTorchNetwork",
+    "BaseClassificationTorchNetwork"
+]
